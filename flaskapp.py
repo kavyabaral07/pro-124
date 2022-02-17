7cdc3e8be5d3a4a060e075b434eb4420ff386dfd
kavyabaral07 <75310574+kavyabaral07@users.noreply.github.com>
2022-02-17 16:14:01 +0530

    Add files via upload

---
+++ flaskapp.py
@@ -0,0 +1,92 @@
+[9:31 PM, 2/13/2022] Whitehat Ma'am: import cv2
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from PIL import Image
+import PIL.ImageOps
+import os, ssl, time
+
+X = np.load('image.npz')['arr_0']
+y = pd.read_csv("labels.csv")["labels"]
+print(pd.Series(y).value_counts())
+classes = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+nclasses = len(classes)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=9, train_size=3500, test_size=500)
+X_train_scaled = X_train/255.0
+X_test_scaled = X_test/255.0
+
+clf = LogisticRegression(solver='saga', multi_class='multinomial').fit(X_train_scaled, y_train)
+
+def get_prediction(image):
+    im_pil = Image.open(image)
+    image_bw = im_pil.convert('L')
+    image_bw_resized = image_bw.resize((28,28), Image.ANTIALIAS)
+    pixel_filter = 20
+    min_pixel = np.percentile(image_bw_resized, pixel_filter)
+    image_bw_resized_inverted_scaled = np.clip(image_bw_resized-min_pixel, 0, 255)
+    max_pixel = np.max(image_bw_resized)
+    image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
+    test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,784)
+    test_pred = clf.predict(test_sample)
+    return test_pred[0]
+[9:31 PM, 2/13/2022] Whitehat Ma'am: classifier.py
+[9:32 PM, 2/13/2022] Whitehat Ma'am: https://raw.githubusercontent.com/pro-whitehatjr/PRO-C125-Project-Solution/main/labels.csv
+[9:35 PM, 2/13/2022] Whitehat Ma'am: project-124, vscode, 1 file only-Flaskapp.py
+[9:35 PM, 2/13/2022] Whitehat Ma'am: from flask import Flask,jsonify, request
+
+app = Flask(_name_)
+
+List = [
+    {
+        'id': 1,
+        'Name': u'Raju',
+        'Contact': u'9987644456', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'Name': u'Rahul',
+        'Contact': u'9876543222', 
+        'done': False
+    }
+]
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/add-data", methods=["POST"])
+def add_task():
+    if not request.json:
+        return jsonify({
+            "status":"error",
+            "message": "Please provide the data!"
+        },400)
+
+    contact = {
+        'id': tasks[-1]['id'] + 1,
+        'Name': request.json['Name'],
+        'Contact': request.json.get('Contact', ""),
+        'done': False
+    }
+    List.append(contact)
+    return jsonify({
+        "status":"success",
+        "message": "Contact added succesfully!"
+    })
+    
+
+@app.route("/get-data")
+def get_task():
+    return jsonify({
+        "data" : List
+    }) 
+
+if (_name_ == "_main_"):
+    app.run(debug=True)
